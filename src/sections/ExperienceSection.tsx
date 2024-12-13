f25712d55d57e4a1598601a2740a13a59e7144ed
martincham <martinchamberlin12@gmail.com>
2024-12-13 18:36:34 -0500

    deploy

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -6,26 +6,21 @@ const ExperienceSection = () => {
       <div className="mx-auto  text-left !text-black p-4">
         <ScrollAnimation>
           <h2 className="ml-[10vw]">Experience</h2>
+          {/* Freelance */}
           <div>
             <div className="flex justify-between">
               <h3 className="">Freelance Software Engineer</h3>
               <h3 className="">Present</h3>
             </div>
-            <p>
-              <a href="https://cosmos.epic.com/">Cosmos</a> team at Epic
-              Systems.
-            </p>
-            <div className="pl-4">
-              <p>
-                •Developed population health analytics software using C#/.NET,
-                SQL, and Typescript
-              </p>
-              <p>
-                • Created distributed-system web services to communicate between
-                servers hosting over 100 million patients
-              </p>
-            </div>
+
+            <ul className="pl-4 list-disc">
+              <li>Building a project management tool with Ruby on Rails</li>
+              <li>
+                Front-end development with React, Tailwind, and Typescript
+              </li>
+            </ul>
           </div>
+          <hr className="my-4 mx-auto h-[1px] bg-black" />
           {/* Epic Systems */}
           <div>
             <div className="flex justify-between">
@@ -36,16 +31,20 @@ const ExperienceSection = () => {
               <a href="https://cosmos.epic.com/">Cosmos</a> team at Epic
               Systems.
             </p>
-            <div className="pl-4">
-              <p>
-                •Developed population health analytics software using C#/.NET,
+            <ul className="pl-4 list-disc">
+              <li>
+                Developed population health analytics software using C#/.NET,
                 SQL, and Typescript
-              </p>
-              <p>
-                • Created distributed-system web services to communicate between
+              </li>
+              <li>
+                Created distributed-system web services to communicate between
                 servers hosting over 100 million patients
-              </p>
-            </div>
+              </li>
+              <li>
+                Researched, designed, and implemented data pipeline for OB/GYN
+                data
+              </li>
+            </ul>
           </div>
           <hr className="my-4 mx-auto h-[1px] bg-black" />
         </ScrollAnimation>
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -15,66 +15,87 @@ const ProjectsSection = () => {
         <h2 className=" ml-[10vw] py-2">Projects</h2>
         <div className="grid opacity-100 grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-8">
           <ScrollAnimation>
-          <Card className="">
-            <CardHeader>
-              <CardTitle>Developer Portfolio</CardTitle>
-              <CardDescription>
-                Built with React.js, Tailwind CSS,
-              </CardDescription>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter className="justify-between">
-              <a href="https://github.com/martincham/martincham.github.io" className="text-blue-600 hover:text-muted-foreground">GitHub -></a>
-              <p className="text-black">2024</p>
-            </CardFooter>
-          </Card>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>Developer Portfolio</CardTitle>
+                <CardDescription>
+                  Built with React.js, Tailwind CSS,
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+              <CardFooter className="justify-between">
+                <a
+                  href="https://github.com/martincham/martincham.github.io"
+                  className="text-blue-600 hover:text-muted-foreground"
+                >
+                  GitHub -{">"}
+                </a>
+                <p className="text-black">2024</p>
+              </CardFooter>
+            </Card>
           </ScrollAnimation>
           <ScrollAnimation>
-          <Card className="">
-            <CardHeader>
-              <CardTitle>Art Portfolio Website</CardTitle>
-              <CardDescription>
-                Built with React.js, Tailwind CSS,
-              </CardDescription>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter className="justify-between">
-              <a href="https://github.com/martincham/martincham.github.io" className="text-blue-600 hover:text-muted-foreground">GitHub -></a>
-              <p className="text-black">2024</p>
-            </CardFooter>
-          </Card>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>Art Portfolio Website</CardTitle>
+                <CardDescription>
+                  Built with React.js, Tailwind CSS,
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+              <CardFooter className="justify-between">
+                <a
+                  href="https://github.com/martincham/martincham.github.io"
+                  className="text-blue-600 hover:text-muted-foreground"
+                >
+                  GitHub -{">"}
+                </a>
+                <p className="text-black">2024</p>
+              </CardFooter>
+            </Card>
           </ScrollAnimation>
           <ScrollAnimation>
-          <Card className="">
-            <CardHeader>
-              <CardTitle>NBA Player Analyzer</CardTitle>
-              <CardDescription>
-                Custom algorithm for evaluating player value. Pushes data to Google Sheets API.
-              </CardDescription>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter className="justify-between">
-              <a href="https://github.com/martincham/espn-nba-fantasy-analyzer" className="text-blue-600 hover:text-muted-foreground">GitHub -></a>
-              <p className="text-black">2023-24</p>
-            </CardFooter>
-          </Card>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>NBA Player Analyzer</CardTitle>
+                <CardDescription>
+                  Custom algorithm for evaluating player value. Pushes data to
+                  Google Sheets API.
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+              <CardFooter className="justify-between">
+                <a
+                  href="https://github.com/martincham/espn-nba-fantasy-analyzer"
+                  className="text-blue-600 hover:text-muted-foreground"
+                >
+                  GitHub -{">"}
+                </a>
+                <p className="text-black">2023-24</p>
+              </CardFooter>
+            </Card>
           </ScrollAnimation>
           <ScrollAnimation>
-          <Card className="">
-            <CardHeader>
-              <CardTitle>Machine Learning Parts Catalog</CardTitle>
-              <CardDescription>
-                Art projects using Machine Learning to create a catalog of useless parts.
-              </CardDescription>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter className="justify-between">
-              <a href="https://github.com/martincham/mcMasterImageMosaic" className="text-blue-600 hover:text-muted-foreground">GitHub -></a>
-              <p className="text-black">2021</p>
-            </CardFooter>
-          </Card>
+            <Card className="">
+              <CardHeader>
+                <CardTitle>Machine Learning Parts Catalog</CardTitle>
+                <CardDescription>
+                  Art projects using Machine Learning to create a catalog of
+                  useless parts.
+                </CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+              <CardFooter className="justify-between">
+                <a
+                  href="https://github.com/martincham/mcMasterImageMosaic"
+                  className="text-blue-600 hover:text-muted-foreground"
+                >
+                  GitHub -{">"}
+                </a>
+                <p className="text-black">2021</p>
+              </CardFooter>
+            </Card>
           </ScrollAnimation>
-
         </div>
       </div>
     </section>
