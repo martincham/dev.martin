52bb3056dd113ed295333ac9108f5f04cd124c8a
martincham <martinchamberlin12@gmail.com>
2024-12-16 21:34:41 -0500

    hmm

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -3,7 +3,7 @@ import ScrollAnimation from "@/components/ui/ScrollAnimation";
 const AboutSection = () => {
   return (
     <section id="about" className="bg-turq ">
-      <div className="mx-auto  text-left text-black p-4">
+      <div className="mx-auto bg-inherit text-left text-black p-4">
         <ScrollAnimation>
           <h2 className="ml-[10vw]">About</h2>
           <p className="mt-4 text-black">
@@ -15,7 +15,7 @@ const AboutSection = () => {
           </p>
           <p className="">MFA in Art, University of Georgia, 2024.</p>
           <hr className="my-4 mx-auto h-[1px] bg-black" />
-          <div className="flex flex-wrap justify-left mt-4 font-medium ">
+          <div className="bg-inherit flex flex-wrap justify-left mt-4 font-medium ">
             <a href="https://www.linkedin.com/in/martincham/">
               <div className="px-2 link">LinkedIn</div>
             </a>
