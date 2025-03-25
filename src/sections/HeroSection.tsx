dac7b8ac23c26f621755cfdaea169f8a0168f4ee
martincham <martinchamberlin12@gmail.com>
2025-03-25 14:35:59 -0400

    change hero line

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -3,10 +3,10 @@ const HeroSection = () => {
     <section id="hero" className="selection:bg-golden">
       <div className="mx-auto mt-[20vh] ">
         <h1
-          style={{ fontSize: "max(2rem, 8.8vw)" }}
+          style={{ fontSize: "max(2rem, 5.8vw)" }}
           className="mx-auto m-0 p-0 font-normal"
         >
-          Martin is optimizing.
+          Martin Chamberlin
         </h1>
         <p style={{ fontSize: "max(1rem, 2vw)" }} className="text mx-auto p-">
           Data | Backend | Frontend | UI/UX | IOT
